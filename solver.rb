daaf94e860a76ade6c19c82022d71412bec59170
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-13 23:49:11 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,7 +1,5 @@
 class Solver
   def factorial(num)
-    # raise 'negative integer' if num.negative?
-
     return 1 if num.zero?
     return num * factorial(num - 1) if num.positive?
   end
@@ -9,17 +7,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
-  
-  def fizzbuzz(num)
-  if (num % 3).zero? && (num % 5).zero?
-    'fizzbuzz'
-  elsif (num % 3).zero?
-    'fizz'
-  elsif (num % 5).zero?
-    'buzz'
-  else
-    num.to_s
-  end
-end
 
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,10 +10,6 @@ describe Solver do
   end
 
   context 'Testing the factorial method' do
-    # it 'should throw exception error if negative interger given' do
-    #   expect(@solver.factorial(-1)).to raise('negative integer')
-    # end
-
     it 'returns 1 if zero interger given' do
       expect(@solver.factorial(0)).to eq(1)
     end
