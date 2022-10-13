74fa658197846a262fe85e015f5aaf939fd65a32
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-13 22:38:42 +0300

    Add the tdd for the factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  it 'should be an instance object of Solver' do
+    expect(@solver).to be_an_instance_of Solver
+  end
+
+  context 'Testing the factorial method' do
+    it 'should throw exception error if negative interger given' do
+      expect(@solver.factorial(-1)).to raise_exception
+    end
+
+    it 'returns 1 if zero interger given' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns 6 if three interger given' do
+      expect(@solver.factorial(3)).to eq(6)
+    end
+  end
+end
